f6ee136476e761681bc4b5fc4af3c2cacfdcde28
YUNUS <yunusemrecogurcu@gmail.com>
2018-04-25 13:57:42 +0100

    fixes4

--- web/WEB-INF/src/LogoutServlet.java
+++ web/WEB-INF/src/LogoutServlet.java
@@ -1,11 +1,13 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet("/LogoutServlet")
 public class LogoutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -13,7 +15,7 @@ public class LogoutServlet extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out=response.getWriter();
 
-        request.getRequestDispatcher("login.jsp").include(request, response);
+        request.getRequestDispatcher("/login.jsp").include(request, response);
 
         HttpSession session = request.getSession();
         session.invalidate();
