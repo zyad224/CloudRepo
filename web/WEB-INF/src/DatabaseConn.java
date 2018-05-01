4cd26e0e2d403be93bcca3fd503b0b287f646422
zyad224 <zyad224@aucegypt.edu>
2018-05-01 12:40:01 +0100

    admins payments book event

--- web/WEB-INF/src/DatabaseConn.java
+++ web/WEB-INF/src/DatabaseConn.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 public class DatabaseConn {
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        String url = "jdbc:mysql://localhost:3306/students";
+        String url = "jdbc:mysql://localhost:3306/STUDENTS?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
         return DriverManager.getConnection(url, "root", "123");
 
     }
